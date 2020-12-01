a45f34c930f9ac774a10b343d1ff6af4591c19be
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-01 18:35:28 +0200

    created the classes

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,31 @@
+import models.DB;
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "odile", "123");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteRangersQuery = "DELETE FROM rangers *;";
+            String deleteLocationsQuery = "DELETE FROM locations *;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            String deleteJoinsRangersQuery = "DELETE FROM rangers_sightings *;";
+            String deleteJoinsLocationsQuery = "DELETE FROM locations_sightings *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteRangersQuery).executeUpdate();
+            con.createQuery(deleteLocationsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+            con.createQuery(deleteJoinsRangersQuery).executeUpdate();
+
+            con.createQuery(deleteJoinsLocationsQuery).executeUpdate();
+        }
+    }
+
+}
