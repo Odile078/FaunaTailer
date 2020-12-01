071dde4eed41c9f6713221245f74ea634b3208cb
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-01 20:12:04 +0200

    Added the variables of the regrangers class and it's getters

--- src/main/java/models/RegRanger.java
+++ src/main/java/models/RegRanger.java
@@ -1,4 +1,34 @@
 package models;
 
+
+
 public class RegRanger {
+    private int id;
+    public String name;
+    public String badge_id ;
+    public String phone;
+
+    public RegRanger (String name, String badge_id, String phone){
+        this.name = name;
+        this.badge_id =badge_id;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBadge_id() {
+        return badge_id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
 }
