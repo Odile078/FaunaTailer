3445fb8ad2650a5fb924509c5dc6904082a06368
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-01 20:19:20 +0200

    Added the save and update methods for the registered ranger

--- src/main/java/models/RegRanger.java
+++ src/main/java/models/RegRanger.java
@@ -1,6 +1,10 @@
 package models;
 
 
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
 
 public class RegRanger {
     private int id;
@@ -30,5 +34,107 @@ public class RegRanger {
         return phone;
     }
 
+    public void save(){
+        try (Connection con=DB.sql2o.open()){
+            String sql="INSERT INTO rangers (name,badge_id,phone) VALUES (:name,:badge_id,:phone)";
+            if(name.equals("")||badge_id.equals("")||phone.equals("")){
+                throw new IllegalArgumentException("fill all fields");
+            }
+            this.id=(int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("badge_id",this.badge_id)
+                    .addParameter("phone",this.phone)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 
+    public static RegRanger find(int id){
+        try (Connection con=DB.sql2o.open()){
+            String sql="SELECT * FROM rangers WHERE id=:id";
+            return con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(RegRanger.class);
+        }
+
+    }
+    public static List<RegRanger> all(){
+        try (Connection con=DB.sql2o.open()){
+            String sql="SELECT * FROM rangers";
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(RegRanger.class);
+
+        }
+
+    }
+    public void update(int id,String name,String phone){
+        try (Connection con=DB.sql2o.open()){
+            String sql="UPDATE rangers SET name=:name,phone=:phone WHERE id=:id";
+            if(name.equals("")||phone.equals("")){
+                throw new IllegalArgumentException("fill all the fields");
+            }
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .addParameter("name",name)
+                    .addParameter("phone",phone)
+                    .executeUpdate();
+
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegRanger regRanger = (RegRanger) o;
+        return id == regRanger.id &&
+                Objects.equals(name, regRanger.name) &&
+                Objects.equals(badge_id, regRanger.badge_id) &&
+                Objects.equals(phone, regRanger.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, badge_id, phone);
+    }
+
+    public void delete(){
+        try (Connection con=DB.sql2o.open()){
+            String sql="DELETE FROM rangers WHERE id=:id";
+
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .executeUpdate();
+        }
+    }
+
+    /*
+    public List<Sighting> getRangerSightings(){
+        try (Connection con=DB.sql2o.open()){
+            String sql="SELECT sighting_id FROM rangers_sightings WHERE ranger_id=:ranger_id";
+            List<Integer> sightings_ids=con.createQuery(sql)
+                    .addParameter("ranger_id",this.getId())
+                    .executeAndFetch(Integer.class);
+            List<Sighting> sightings=new ArrayList<Sighting>();
+
+            for(Integer sighting_id:sightings_ids){
+                String sightingsQuery="SELECT * FROM sightings WHERE id=:sighting_id";
+                Sighting sighting=con.createQuery(sightingsQuery)
+                        .addParameter("sighting_id",sighting_id)
+                        .executeAndFetchFirst(Sighting.class);
+                sightings.add(sighting);
+
+            }
+            if(sightings.size()==0){
+                throw new IllegalArgumentException("Ranger has no sighting");
+            }
+            else {return sightings;}
+
+
+        }
+
+    }
+            */
 }
