52d5f985c0f27e500f4ab8f9f8ba49b4f3e8f43a
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-01 19:52:29 +0200

    creating the variables in RegEndangered animal and it's constructor

--- src/main/java/models/RegAnimal.java
+++ src/main/java/models/RegAnimal.java
@@ -1,6 +1,12 @@
 package models;
 
-public class RegAnimal {
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+import java.util.Objects;
+
+public class RegAnimal implements DatabaseManagement{
     public int id;
     public String name;
     public String type;
@@ -31,7 +37,114 @@ public class RegAnimal {
     public String getAge() {
         return age;
     }
+
     public int getId() {
         return id;
+    }
+
+    public void save(){
+        if(this.name.equals("")||this.type.equals("")||this.name.equals(null)||this.type.equals(null)){
+            throw new IllegalArgumentException("Fill all the fields");
+        }
+        try (Connection con=DB.sql2o.open()){
+
+
+            String sql ="INSERT INTO animals (name,type) VALUES (:name,:type)";
+
+            this.id=(int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+    public static RegAnimal find(int id){
+        try (Connection con=DB.sql2o.open()){
+            String sql= "SELECT * FROM animals WHERE id=:id";
+            RegAnimal animal=  con.createQuery(sql)
+                    .addParameter("id",id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(RegAnimal.class);
+            return animal;
+
+        }
+
+    }
+    public static List<RegAnimal> all(){
+        try (Connection con=DB.sql2o.open()) {
+            String sql ="SELECT * FROM animals";
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(RegAnimal.class);
+
+        }
+    }
+    public void update(int id,String type,String health,String age) {
+        try (Connection con = DB.sql2o.open()) {
+            if (type.equals("")) {
+                throw new IllegalArgumentException("All fields must be filled");
+            }
+            if (type == "endangered") {
+                if (health.equals("") || age.equals("")) {
+                    throw new IllegalArgumentException("All fields must be filled");
+                }
+                String sql = "UPDATE animals SET type=:type,health=:health,age=:age WHERE id=:id";
+                con.createQuery(sql)
+                        .addParameter("type", type)
+                        .addParameter("health", health)
+                        .addParameter("age", age)
+                        .addParameter("id", this.id)
+                        .executeUpdate();
+            } else {
+
+                String sql = "UPDATE animals SET type=:type,health=:health,age=:age WHERE id=:id";
+                con.createQuery(sql)
+                        .addParameter("type", type)
+                        .addParameter("health", "")
+                        .addParameter("age", "")
+                        .addParameter("id", this.id)
+                        .executeUpdate();
+            }
+
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegAnimal regAnimal = (RegAnimal) o;
+        return id == regAnimal.id &&
+                Objects.equals(name, regAnimal.name) &&
+                Objects.equals(type, regAnimal.type) &&
+                Objects.equals(health, regAnimal.health) &&
+                Objects.equals(age, regAnimal.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, health, age);
+    }
+
+    public void delete(){
+        try (Connection con=DB.sql2o.open()){
+            String sql = "DELETE FROM animals WHERE id=:id";
+            con.createQuery(sql)
+                    .addParameter("id",this.id)
+                    .executeUpdate();
+
+        }
+    }
+    public static void deleteAll() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals";
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 }
--- src/main/java/models/RegEndangeredAnimal.java
+++ src/main/java/models/RegEndangeredAnimal.java
@@ -1,4 +1,23 @@
 package models;
 
-public class RegEndangeredAnimal {
+public class RegEndangeredAnimal extends RegAnimal{
+    public String health;
+    public String age;
+
+    public static final String HEALTH_HEALTHY="healthy";
+    public static final String HEALTH_ILL="ill";
+    public static final String HEALTH_OKAY="okay";
+
+    public static final String AGE_NEWBORN="newborn";
+    public static final String AGE_YOUNG="young";
+    public static final String AGE_ADULT="adult";
+
+    public static final String ANIMAL_TYPE="endangered";
+    public RegEndangeredAnimal(String name,String type,String health,String age) {
+        super(name,type);
+        this.type=type;
+        this.health=health;
+        this.age=age;
+
+    }
 }
